393f272c0aee5e8bb1764d11aa7e82e60d801868
Jorge-Martinez82 <jmartines@gmail.com>
2025-08-22 10:58:26 +0200

    feat: implement routing and header navigation structure

--- Angular/ProyectoTwinFrontend/src/app/app-routing-module.ts
+++ Angular/ProyectoTwinFrontend/src/app/app-routing-module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { Home } from './pages/home/home';
+import { Monitor } from './pages/monitor/monitor';
+import { About } from './pages/about/about';
+
+const routes: Routes = [
+  { path: '', component: Home },
+  { path: 'home', component: Home },
+  { path: 'monitor', component: Monitor },
+  { path: 'about', component: About },
+  { path: '**', redirectTo: '', pathMatch: 'full' }    // Redirige a Home
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
