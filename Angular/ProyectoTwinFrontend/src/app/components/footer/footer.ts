fad31e6cb6025d70bec7022f1cd31f6a62cd9f99
Jorge-Martinez82 <jmartines@gmail.com>
2025-08-22 10:44:15 +0200

    feat: initialize Angular project with main components and routing
    - Created main application component (App) with title signal.
    - Added footer component with basic structure and test cases.
    - Added header component with basic structure and test cases.
    - Implemented index.html to bootstrap the application.
    - Configured main.ts for Angular module bootstrapping.
    - Set up TypeScript configuration files for app and spec.
    - Added global styles in styles.css.
    - Created unit tests for App, Header, and Footer components.

---
+++ Angular/ProyectoTwinFrontend/src/app/components/footer/footer.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'tw-footer',
+  standalone: false,
+  templateUrl: './footer.html',
+  styleUrl: './footer.css'
+})
+export class Footer {
+
+}
---
+++ Angular/ProyectoTwinFrontend/src/app/components/header/header.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'tw-header',
+  standalone: false,
+  templateUrl: './header.html',
+  styleUrl: './header.css'
+})
+export class Header {
+
+}
