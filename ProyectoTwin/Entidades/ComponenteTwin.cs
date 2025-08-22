efc25bcc91f76342846ad6c9f7d3c68dc7c26658
Jorge-Martinez82 <jmartines@gmail.com>
2025-08-22 09:19:22 +0200

    Añadir validaciones a ComponenteTwin
    Se han agregado atributos de validación a las propiedades de la clase `ComponenteTwin`, incluyendo `[Key]`, `[Required]` y `[StringLength(n)]`. También se ha añadido la propiedad `MantenimientoProgramado` de tipo `bool`.

--- ProyectoTwin/Entidades/ComponenteTwin.cs
+++ ProyectoTwin/Entidades/ComponenteTwin.cs
@@ -1,16 +1,35 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace ProyectoTwin.Entidades
 {
     public class ComponenteTwin
     {
+        [Key]
         public int IdComponente { get; set; }
+
+        [Required]
+        [StringLength(100)]
         public string Nombre { get; set; }
+
+        [Required]
+        [StringLength(50)]
         public string TipoDatos { get; set; }
+
+        [Required]
+        [StringLength(50)]
         public string EstadoReal { get; set; }
+
+        [Required]
+        [StringLength(50)]
         public string EstadoDigital { get; set; }
+
+        [Required]
         public DateTime UltimaActualizacion { get; set; }
+
+        [StringLength(100)]
         public string IndicadorSostenibilidad { get; set; }
+
         public bool MantenimientoProgramado { get; set; }
     }
 }
